b548f33b6fb06c82b0dbd075fbb7f9852c687580
Raj Aryan <100435391+raj4kri@users.noreply.github.com>
2024-04-01 07:59:32 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-// import logo from './logo.svg';
-import './App.css';
-import Home from './componenets/Home/Home.js';
+import "./App.css";
+
+import Navbar from "./componenets/Navbar/Navbar";
+import Card from "./componenets/Card/Card";
+import Card2 from "./componenets/Card2/Card2";
+import Card3 from "./componenets/Card3/Card3";
+import Card4 from "./componenets/Card4/Card4";
+
+// import Search from '../Section/Section'
+import Section from "./componenets/Section/Section";
+import Footer from "./componenets/Footer";
 
 function App() {
   return (
-  
-    <Home/>
-    
-  
+    <div className="container">
+      <Navbar />
+
+      <Section />
+      <Card />
+      <Card2 />
+      <Card3 />
+      <Card4 />
+      <Footer />
+    </div>
   );
 }
 
